61fd1e2c900127bd3aceb80c36d01af13b9b8c81
student <student@ip-172-31-63-243.us-west-2.compute.internal>
2021-02-08 12:19:08 -0800

    Commitint in three... two... one... (WHOOSH)

---
+++ src/ObjectManager.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ObjectManager implements ActionListener{
+Covid_Connor TheTwin;
+Random random = new Random();
+ArrayList<CovidBlob> aliens = new ArrayList<>();
+ObjectManager(Covid_Connor TheTwin){
+	this.TheTwin = TheTwin;
+}
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+}
